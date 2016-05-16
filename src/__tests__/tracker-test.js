4e094df1f24adf3e034bf351a6faa93b268b26c6
Chiyu Zhong <1606069+CatTail@users.noreply.github.com>
2016-05-16 14:47:21 +0800

    test BINARY_FORMAT

--- src/__tests__/tracker-test.js
+++ src/__tests__/tracker-test.js
@@ -7,6 +7,7 @@ let tracer = new Tracer()
 tracer.setInterface(inf)
 
 const FORMAT_TEXT_MAP = inf.FORMAT_TEXT_MAP
+const FORMAT_BINARY = inf.FORMAT_BINARY
 
 const OPERATION_NAME = 'basictracer-test'
 const ANOTHER_OPERATION_NAME = 'another-basictracer-test'
@@ -31,11 +32,27 @@ describe('Tracer', () => {
     it('should join receving span', () => {
         // inject
         let parentSpan = tracer.startSpan({operationName: OPERATION_NAME})
-        let carrier = {baggage: {key: 'value'}}
+        let carrier = {}
         tracer.inject(parentSpan, FORMAT_TEXT_MAP, carrier)
 
         // join
         let span = tracer.join(ANOTHER_OPERATION_NAME, FORMAT_TEXT_MAP,
+                               carrier)
+        should(span.traceId.equals(parentSpan.traceId)).be.ok()
+        should(span.spanId.equals(parentSpan.spanId)).be.not.ok()
+        should(span.parentId.equals(parentSpan.spanId)).be.ok()
+        should(span.sampled).eql(parentSpan.sampled)
+        should(span.baggage).eql(parentSpan.baggage)
+    })
+
+    it('should join binary span', () => {
+        // inject
+        let parentSpan = tracer.startSpan({operationName: OPERATION_NAME})
+        let carrier = {}
+        tracer.inject(parentSpan, FORMAT_BINARY, carrier)
+
+        // join
+        let span = tracer.join(ANOTHER_OPERATION_NAME, FORMAT_BINARY,
                                carrier)
         should(span.traceId.equals(parentSpan.traceId)).be.ok()
         should(span.spanId.equals(parentSpan.spanId)).be.not.ok()
